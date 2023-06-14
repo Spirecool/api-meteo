27bc715485402758c4d5948ef044090ac8f025f1
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-06-14 14:29:22 +0200

    Adding JS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let ville = 'Paris';
+let ville = 'Orléans';
 
 // Afficher à l'entrée de la page une température d'une ville par défaut, ici Paris
 // Pour ca on appelle la fonction recevoirTemperature();
@@ -7,22 +7,24 @@ recevoirTemperature(ville);
 
 function recevoirTemperature(ville) {
 
-
-let appid = '5dd4a3cfb9e347e4e1b4e7fd61fe21dd';
+// Clé API
+let appid = '166472416bdbf564b04d6b39c02e5558';
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville +'&appid='+ appid + '&lang=fr&units=metric';
 
+// Objet du navigateur accessible en JavaScript qui permet d'obtenir des données au format XML, JSON
 let requete = new XMLHttpRequest();
 
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
+requete.open('GET', url); // on veut décupérer des informations + url ressources à obtenir
+requete.responseType = 'json'; // Type de réponse
+requete.send(); // on envoie la requête
 
-requete.onload = function() {
+requete.onload = function() { // au chargement de la requête
     console.log(requete);
-        if (requete.readyState == XMLHttpRequest.DONE) {    
-            if (requete.status === 200) {
-                let temp = requete.response.main.temp;
+    if (requete.readyState == XMLHttpRequest.DONE) {    
+        // if (requete.readyState == XMLHttpRequest.DONE) {     // si c'est 4 c'est que ça fonctionne 
+            if (requete.status === 200) { // si c'est 200 on commence toutes les instructions
+                let temp = requete.response.main.temp; // dans la console est dans XMLHttpRequest/response/main/temp
                 let ville =  requete.response.name;
                 let icone = requete.response.weather[0].icon;
                 let min = requete.response.main.temp_min;
@@ -34,17 +36,22 @@ requete.onload = function() {
                 let img = document.createElement('img');
                 img.src = 'https://openweathermap.org/img/wn/'+icone+'@2x.png';
 
-                let selectVille = document.querySelector('#ville')
-                selectVille.innerHTML = '<h2>' + ville + '</h2>';
+                let selectVille = document.querySelector('#ville') // on sélectionne l'id #ville (changer de ville)
+                selectVille.innerHTML = '<h2>' + ville + '</h2>'; // on la remplace par la ville qu'on a saisi
 
                 selectVille.append(img);
                 selectVille.append(newDiv);
 
-                let selectTemp = document.querySelector('#temperature_label')
-                selectTemp.textContent = temp;
+                let selectTemp = document.querySelector('#temperature_label') // on sélectionne l'id #temperature_label 
+                selectTemp.textContent = temp; // on le remplace par la température de la ville sélectionnée
             }    
             else {
-                alert('Un problème est intervenu, merci de ressaisir une nouvelle ville ou revenir plus tard.');
+                let error = document.querySelector('#error');
+                error.innerHTML='Un problème est intervenu, merci de ressaisir une nouvelle ville ou de revenir plus tard.'
+                error.style.color = 'red'
+                error.style.fontWeight = 'bolder'
+                error.style.marginTop = '20px'
+
             }    
         }    
         
@@ -54,8 +61,10 @@ requete.onload = function() {
 let bouton = document.querySelector('#changer');
 bouton.addEventListener('click', () => {
 
-    let villeChoisie = ville;
+    let villeChoisie = ville; // ville = c'est la ville en paramètre de la fonction recevoirTemperature()
 
     villeChoisie = prompt('Quelle ville souhaitez-vous choisir ?');
+    
     recevoirTemperature(villeChoisie);
 })
+
